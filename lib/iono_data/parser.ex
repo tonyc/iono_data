a32a228191768d9bb3d2cbb064ee33251b687cbc
Tony Collen <tony@halogenlabs.com>
2016-08-03 20:19:20 -0500

    Change parser to skip 'no data available' line

--- lib/iono_data/parser.ex
+++ lib/iono_data/parser.ex
@@ -3,11 +3,19 @@ defmodule IonoData.Parser do
     IO.puts "Reading file: #{filename}"
 
     File.stream!(filename)
-    |> Stream.filter_map(&non_comments/1, &IonoData.Record.from_line/1)
+    |> Stream.filter_map(&read_line?/1, &IonoData.Record.from_line/1)
     |> Enum.each(&IO.inspect/1)
+  end
+
+  defp read_line?(line) do
+    non_comments(line) && no_data?(line)
   end
 
   defp non_comments(line) do
     !String.match?(line, ~r/[:#]/)
+  end
+
+  defp no_data?(line) do
+    !String.match?(line, ~r/^No data available/)
   end
 end
--- lib/mix/tasks/parse_iono.ex
+++ lib/mix/tasks/parse_iono.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.ParseIono do
   use Mix.Task
 
+  @shortdoc "Parses Iono Data"
+  
   def run(args \\ []) do
     filename = List.first(args) || "sample_data/Dourbes_iono.txt"
 
