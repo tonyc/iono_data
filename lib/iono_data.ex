58a7b1074554966981bca6a3df199b3c45f7dd89
Tony Collen <tony@halogenlabs.com>
2016-08-09 20:06:57 -0500

    import supervisor config from ecto docs, unignore mix.lock

--- lib/iono_data.ex
+++ lib/iono_data.ex
@@ -1,9 +1,20 @@
 defmodule IonoData do
+  use Application
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Friends.Worker, [arg1, arg2, arg3]),
       worker(Iono.Repo, [])
     ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: IonoData.Supervisor]
+    Supervisor.start_link(children, opts)
+
+
   end
 end
